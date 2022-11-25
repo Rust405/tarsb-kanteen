a95f73cb1f65bde33297440c6face15999e19220
Russell Len <russelllen405@gmail.com>
2022-11-25 15:27:19 +0800

    Added loading circle to Loading

--- src/Loading.js
+++ src/Loading.js
@@ -1,11 +1,17 @@
 import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
+import CircularProgress from '@mui/material/CircularProgress'
+import Stack from '@mui/material/Stack'
 
 const Loading = ({ loadingMsg }) => {
     return (
         <div className="loading">
             <Box sx={{ width: '100%' }} display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-                <Typography variant="body">{loadingMsg}</Typography>
+                <Stack alignItems="center" spacing={2}>
+                    <Typography variant="body">{loadingMsg}</Typography>
+                    <CircularProgress />
+                </Stack>
+
             </Box>
         </div>)
 }
