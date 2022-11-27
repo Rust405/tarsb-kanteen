0adeda0981a22ca0cc40122dc3b34557d7a85e5b
Russell Len <russelllen405@gmail.com>
2022-11-27 23:40:31 +0800

    Added fixed width to updatestall page

--- src/user-pages/stall/StallSettings/index.js
+++ src/user-pages/stall/StallSettings/index.js
@@ -19,6 +19,8 @@ import RemoveIcon from '@mui/icons-material/Remove'
 import { closeStall, openStall, updateStallDetails } from '../../../utils/firebase'
 import { Alert } from '../../../utils/customComponents'
 
+const stallSettingsWidth = 325
+
 const StallSettings = ({ stallSnapshot, stallDocRef, stallID }) => {
 
     const [disableSwitch, setDisableSwitch] = useState(false)
@@ -133,106 +135,109 @@ const StallSettings = ({ stallSnapshot, stallDocRef, stallID }) => {
 
     return (
         <div className="stall-settings">
-            <Box sx={{ m: 2 }}>
-                <Typography variant="h6" gutterBottom>Stall Status</Typography>
-
-                <Stack direction="row" alignItems="center" spacing={2}>
-                    <Switch
-                        checked={stallSnapshot.status === 'open'}
-                        onChange={handleStatusToggle}
-                        disabled={disableSwitch}
-                    />
-                    <Typography variant="body1">
-                        The stall is now {stallSnapshot.status} and {stallSnapshot.status === 'open' ? "accepting" : "not accepting"} orders.
-                    </Typography>
-                </Stack>
-            </Box>
-
-            <Divider />
-
-            <Box sx={{ m: 2 }}>
-                <Typography variant="h6" gutterBottom>Stall Details {isEditing && "(Editing)"}</Typography>
-
+            <Box sx={{ width: stallSettingsWidth }}>
                 <Stack spacing={2}>
-                    <TextField label="Stall Name" variant="outlined" size="small" autoComplete='off'
-                        value={stallName} onChange={(e) => setStallName(e.target.value)} disabled={!isEditing || isValidating} />
+                    <div>
+                        <Typography variant="h6" gutterBottom>Stall Status</Typography>
 
-                    <Typography>Staff Emails ({staffEmails.length === 0 ? "None" : staffEmails.length}) </Typography>
-
-                    {staffEmails.length !== 0 &&
-                        <Paper style={{ maxHeight: 128, overflow: 'auto' }}>
-                            <List>
-                                {staffEmails.map(
-                                    (staffEmail, index) => (
-                                        <ListItem key={index}
-                                            disabled={!isEditing || isValidating}
-                                            secondaryAction={
-                                                isEditing && !isValidating &&
-                                                <IconButton
-                                                    onClick={() => handleRemoveStaff(index)} >
-                                                    <RemoveIcon />
-                                                </IconButton>
-                                            }>
-                                            {staffEmail}
-                                        </ListItem>
-                                    )
-                                )}
-                            </List>
-                        </Paper>
-                    }
-
-                    {staffEmails.length <= 9 && isEditing &&
-                        <Stack direction="row" alignItems="center" spacing={1}>
-                            <TextField label="Staff Email" variant="outlined" size="small" autoComplete='off'
-                                value={newStaffEmail}
-                                onChange={(e) => setNewStaffEmail(e.target.value)}
-                                disabled={isValidating}
-                                inputProps={{ style: { textTransform: "lowercase" } }}
-                                onKeyPress={(e) => { if (e.key === 'Enter') handleAddStaff() }}
-                            />
-                            <Button onClick={handleAddStaff} disabled={newStaffEmail.trim() === '' || isValidating}>Add</Button>
-                        </Stack>
-                    }
-
-                    {/* Enable editing button */}
-                    {!isEditing &&
-                        <Box display="flex" justifyContent="center">
-                            <Button onClick={() => setIsEditing(true)}>Edit Stall Details</Button>
-                        </Box>
-                    }
-
-                    {/* Details Edit buttons */}
-                    {isEditing &&
                         <Stack direction="row" alignItems="center" spacing={2}>
-                            <Button onClick={handleRemoveChanges} disabled={isValidating} >Cancel</Button>
-                            <Box sx={{ position: 'relative' }}>
-                                <Button onClick={handleSaveChanges} disabled={isValidating}>
-                                    {isValidating ? "Validating..." : "Save Changes"}
-                                </Button>
-                                {isValidating &&
-                                    <CircularProgress
-                                        size={24}
-                                        sx={{
-                                            position: 'absolute',
-                                            top: '50%',
-                                            left: '50%',
-                                            marginTop: '-12px',
-                                            marginLeft: '-12px',
-                                        }}
-                                    />
-                                }
-                            </Box>
+                            <Switch
+                                checked={stallSnapshot.status === 'open'}
+                                onChange={handleStatusToggle}
+                                disabled={disableSwitch}
+                            />
+                            <Typography variant="body1">
+                                The stall is now {stallSnapshot.status} and {stallSnapshot.status === 'open' ? "accepting" : "not accepting"} orders.
+                            </Typography>
                         </Stack>
-                    }
+                    </div>
 
+                    <Divider />
+
+                    <div>
+                        <Typography variant="h6" gutterBottom>Stall Details {isEditing && "(Editing)"}</Typography>
+
+                        <Stack spacing={2}>
+                            <TextField label="Stall Name" variant="outlined" size="small" autoComplete='off'
+                                value={stallName} onChange={(e) => setStallName(e.target.value)} disabled={!isEditing || isValidating} />
+
+                            <Typography>Staff Emails ({staffEmails.length === 0 ? "None" : staffEmails.length}) </Typography>
+
+                            {staffEmails.length !== 0 &&
+                                <Paper style={{ maxHeight: 128, overflow: 'auto' }}>
+                                    <List>
+                                        {staffEmails.map(
+                                            (staffEmail, index) => (
+                                                <ListItem key={index}
+                                                    disabled={!isEditing || isValidating}
+                                                    secondaryAction={
+                                                        isEditing && !isValidating &&
+                                                        <IconButton
+                                                            onClick={() => handleRemoveStaff(index)} >
+                                                            <RemoveIcon />
+                                                        </IconButton>
+                                                    }>
+                                                    {staffEmail}
+                                                </ListItem>
+                                            )
+                                        )}
+                                    </List>
+                                </Paper>
+                            }
+
+                            {staffEmails.length <= 9 && isEditing &&
+                                <Stack direction="row" alignItems="center" spacing={1}>
+                                    <TextField label="Staff Email" variant="outlined" size="small" autoComplete='off'
+                                        value={newStaffEmail}
+                                        onChange={(e) => setNewStaffEmail(e.target.value)}
+                                        disabled={isValidating}
+                                        inputProps={{ style: { textTransform: "lowercase" } }}
+                                        onKeyPress={(e) => { if (e.key === 'Enter') handleAddStaff() }}
+                                    />
+                                    <Button onClick={handleAddStaff} disabled={newStaffEmail.trim() === '' || isValidating}>Add</Button>
+                                </Stack>
+                            }
+
+                            {/* Enable editing button */}
+                            {!isEditing &&
+                                <Box display="flex" justifyContent="center">
+                                    <Button onClick={() => setIsEditing(true)}>Edit Stall Details</Button>
+                                </Box>
+                            }
+
+                            {/* Details Edit buttons */}
+                            {isEditing &&
+                                <Stack direction="row" alignItems="center" spacing={2}>
+                                    <Button onClick={handleRemoveChanges} disabled={isValidating} >Cancel</Button>
+                                    <Box sx={{ position: 'relative' }}>
+                                        <Button onClick={handleSaveChanges} disabled={isValidating}>
+                                            {isValidating ? "Validating..." : "Save Changes"}
+                                        </Button>
+                                        {isValidating &&
+                                            <CircularProgress
+                                                size={24}
+                                                sx={{
+                                                    position: 'absolute',
+                                                    top: '50%',
+                                                    left: '50%',
+                                                    marginTop: '-12px',
+                                                    marginLeft: '-12px',
+                                                }}
+                                            />
+                                        }
+                                    </Box>
+                                </Stack>
+                            }
+
+                        </Stack>
+                    </div>
+
+                    <Divider />
+
+                    <Box display="flex" justifyContent="center">
+                        <Button variant="outlined" size="small" color="error" onClick={handleUnregister}>Unregister Stall</Button>
+                    </Box>
                 </Stack>
-
-            </Box>
-
-            <Divider />
-
-            <Box sx={{ m: 2 }} display="flex" justifyContent="center">
-                <Button variant="outlined" color="error" onClick={handleUnregister}>Unregister Stall</Button>
             </Box>
 
             {/* Changes saved snackbar */}
@@ -253,7 +258,6 @@ const StallSettings = ({ stallSnapshot, stallDocRef, stallID }) => {
                     }
                 </Alert>
             </Snackbar>
-
         </div>
     )
 }
