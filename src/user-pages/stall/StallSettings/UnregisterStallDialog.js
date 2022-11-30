fd8e8426930d8fa30b957d6fa85c9022f65305ab
Russell Len <russelllen405@gmail.com>
2022-11-30 23:52:39 +0800

    Added error snack to unregister stall

--- src/user-pages/stall/StallSettings/UnregisterStallDialog.js
+++ src/user-pages/stall/StallSettings/UnregisterStallDialog.js
@@ -9,22 +9,33 @@ import DialogContentText from '@mui/material/DialogContentText'
 import DialogTitle from '@mui/material/DialogTitle'
 import Typography from '@mui/material/Typography'
 import Box from '@mui/material/Box'
+import Snackbar from '@mui/material/Snackbar'
 
+import { Alert } from '../../../utils/customComponents'
 import { unregisterStall } from '../../../utils/firebase'
 
 const UnregisterStallDialog = ({ openDialog, setOpenDialog, stallName, stallID }) => {
-
     const [confirmation, setConfirmation] = useState('')
 
     const handleUnregisterStall = () => {
+        setIsUnregistering(true)
         unregisterStall({ stallID: stallID })
             .catch(err => {
                 console.warn(err)
-                //popup
+                setOpenErrSnack(true)
+                setIsUnregistering(false)
             })
     }
 
+    const [isUnregistering, setIsUnregistering] = useState(false)
+    const [openErrSnack, setOpenErrSnack] = useState(false)
+    const handleCloseErrSnack = (event, reason) => {
+        if (reason === 'clickaway') return
+        setOpenErrSnack(false)
+    }
+
     const handleCloseDialog = () => {
+        if (isUnregistering) return
         setConfirmation('')
         setOpenDialog(false)
     }
@@ -36,22 +47,29 @@ const UnregisterStallDialog = ({ openDialog, setOpenDialog, stallName, stallID }
 
                 <DialogContent>
                     <DialogContentText id="alert-dialog-description">
-                        Before your proceed, please note that all users 
+                        Before your proceed, please note that all users
                         associated with the stall (incluyding yourself) will be automatically logged out.
                         This process cannot be undone.
                     </DialogContentText>
 
                     <Box sx={{ m: 2 }}>
                         <Typography> Confirm the unregistration of this stall by typing its name: <strong>{stallName}</strong></Typography>
-                        <TextField size="small" value={confirmation} onChange={(e) => setConfirmation(e.target.value)} placeholder={stallName} />
+                        <TextField size="small" value={confirmation} disabled={isUnregistering}
+                            onChange={(e) => setConfirmation(e.target.value)} placeholder={stallName} />
                     </Box>
                 </DialogContent>
 
                 <DialogActions>
-                    <Button onClick={handleCloseDialog}>Cancel</Button>
-                    <Button onClick={handleUnregisterStall} disabled={confirmation !== stallName} autoFocus>Proceed</Button>
+                    <Button onClick={handleCloseDialog} disabled={isUnregistering}>Cancel</Button>
+                    <Button onClick={handleUnregisterStall} disabled={confirmation !== stallName || isUnregistering} autoFocus>Proceed</Button>
                 </DialogActions>
             </Dialog>
+
+            {/* Error messages snackbar */}
+            <Snackbar open={openErrSnack} autoHideDuration={5000} onClose={handleCloseErrSnack}
+                anchorOrigin={{ vertical: "top", horizontal: "right" }}>
+                <Alert onClose={handleCloseErrSnack} severity="error" sx={{ width: '100%' }}>Unable to proceed. A server error has occured.</Alert>
+            </Snackbar>
         </div >
     )
 }
