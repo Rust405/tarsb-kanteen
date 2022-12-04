497c8cfa1c3e1750e16fca06ed563ded0c62f627
Russell Len <russelllen405@gmail.com>
2022-12-04 18:56:50 +0800

    Updated login page

--- src/Login.js
+++ src/Login.js
@@ -7,7 +7,8 @@ import Box from '@mui/material/Box'
 import Typography from '@mui/material/Typography'
 import Stack from '@mui/material/Stack'
 
-import GoogleIcon from '@mui/icons-material/Google';
+import GoogleIcon from '@mui/icons-material/Google'
+import InfoIcon from '@mui/icons-material/Info'
 
 const Login = () => {
     const navigate = useNavigate()
@@ -17,13 +18,17 @@ const Login = () => {
     return (
         <div className="login">
             <Box sx={{ width: '100%' }} display="flex" justifyContent="center" alignItems="center" minHeight="100vh">
-                <Stack sx={{ m: 2 }} spacing={4}>
+                <Stack sx={{ m: 2 }} spacing={3}>
                     <Typography variant="h2" align="center" sx={{ fontWeight: 'bold' }}>TARSB<br />Kanteen</Typography>
                     <Typography variant="body" align="center">Log in with your Google account to get started!</Typography>
+                    <Stack direction="row" alignItems="center" spacing={1}>
+                        <InfoIcon /><Typography variant="caption">If you are a TAR student or lecturer/staff, use your email ending in tarc.edu.my.</Typography>
+                    </Stack>
 
                     <Box display="flex" justifyContent="center">
                         <Button startIcon={<GoogleIcon />} variant="contained" onClick={signInWithGoogle} >Login with Google</Button>
                     </Box>
+
                 </Stack>
             </Box>
         </div>
