857c49b1e3636f83831263aa1dd4b9b94937ddc9
Russell Len <russelllen405@gmail.com>
2022-12-14 15:03:02 +0800

    Made swipeable edge primary color Changed disable render to disable vis for on navopen

--- src/app-components/MultiPurposeSidebar.js
+++ src/app-components/MultiPurposeSidebar.js
@@ -40,74 +40,74 @@ const MultiPurposeSidebar = ({
             </Drawer>
 
             {/* Mobile Drawer */}
-            {!navOpen &&
-                <SwipeableDrawer
-                    container={container}
-                    anchor="bottom"
-                    open={sidebarOpen}
-                    onOpen={handleSidebarToggle}
-                    onClose={handleSidebarToggle}
-                    swipeAreaWidth={drawerBleeding}
-                    disableSwipeToOpen={false}
-                    ModalProps={{
-                        keepMounted: true
-                    }}
+
+            <SwipeableDrawer
+                container={container}
+                anchor="bottom"
+                open={sidebarOpen}
+                onOpen={handleSidebarToggle}
+                onClose={handleSidebarToggle}
+                swipeAreaWidth={drawerBleeding}
+                disableSwipeToOpen={false}
+                ModalProps={{
+                    keepMounted: true
+                }}
+                sx={{
+                    display: {
+                        xs: !navOpen ? 'block' : 'none',
+                        sm: 'none'
+                    },
+                    '& .MuiDrawer-paper': {
+                        height: `calc(75% - ${drawerBleeding}px)`,
+                        overflow: 'visible',
+                    }
+                }}
+            >
+
+                {/* Sidebar swipeable edge */}
+                <Box
                     sx={{
-                        display: {
-                            xs: 'block',
-                            sm: 'none'
-                        },
-                        '& .MuiDrawer-paper': {
-                            height: `calc(75% - ${drawerBleeding}px)`,
-                            overflow: 'visible',
-                        }
+                        backgroundColor: 'primary.main',
+                        position: 'absolute',
+                        top: -drawerBleeding,
+                        borderTopLeftRadius: 8,
+                        borderTopRightRadius: 8,
+                        visibility: 'visible',
+                        right: 0,
+                        left: 0
                     }}
                 >
-
-                    {/* Sidebar swipeable edge */}
+                    {/* Puller */}
                     <Box
                         sx={{
-                            backgroundColor: '#fff',
+                            width: 30,
+                            height: 6,
+                            backgroundColor: grey[300],
+                            borderRadius: 3,
                             position: 'absolute',
-                            top: -drawerBleeding,
-                            borderTopLeftRadius: 8,
-                            borderTopRightRadius: 8,
-                            visibility: 'visible',
-                            right: 0,
-                            left: 0
+                            top: 8,
+                            left: 'calc(50% - 15px)'
                         }}
-                    >
-                        {/* Puller */}
-                        <Box
-                            sx={{
-                                width: 30,
-                                height: 6,
-                                backgroundColor: grey[300],
-                                borderRadius: 3,
-                                position: 'absolute',
-                                top: 8,
-                                left: 'calc(50% - 15px)'
-                            }}
-                        />
-                        <Typography sx={{ p: 2, color: 'text.secondary' }}>TODO:</Typography>
-                    </Box>
+                    />
+                    <Typography sx={{ p: 2, color: 'white' }}>TODO:</Typography>
+                </Box>
 
-                    {/* Sidebar content */}
-                    <Box
-                        sx={{
-                            backgroundColor: '#fff',
-                            px: 2,
-                            pb: 2,
-                            height: '100%',
-                            overflow: 'auto'
-                        }}
-                    >
-                        {drawerContent}
-                    </Box>
-                </SwipeableDrawer>
-            }
+                {/* Sidebar content */}
+                <Box
+                    sx={{
+                        backgroundColor: '#fff',
+                        px: 2,
+                        pb: 2,
+                        height: '100%',
+                        overflow: 'auto'
+                    }}
+                >
+                    {drawerContent}
+                </Box>
+            </SwipeableDrawer>
+
         </div>
-    );
+    )
 }
 
-export default MultiPurposeSidebar;
+export default MultiPurposeSidebar
