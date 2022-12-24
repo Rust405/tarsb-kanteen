219ff94af959b177b5b161e93a78bae54c857341
Russell Len <russelllen405@gmail.com>
2022-12-24 20:13:37 +0800

    Fixed how login,loading,offline are centered

--- src/Login.js
+++ src/Login.js
@@ -16,17 +16,20 @@ const Login = () => {
     useEffect(() => navigate('/'), [])
 
     return (
-        <div className="login">
-            <Box sx={{ width: '100%' }} display="flex" justifyContent="center" alignItems="center" minHeight="calc(100vh - 16px)">
-                <Stack sx={{ m: 2 }} spacing={3}>
-                    <Typography variant="h2" align="center" sx={{ fontWeight: 'bold' }}>TARSB<br />Kanteen</Typography>
-                    <Typography variant="body" align="center">Log in with your Google account to get started!</Typography>
-                    <Stack direction="row" alignItems="center" spacing={1}>
-                        <InfoIcon /><Typography variant="caption">If you are a TAR student or lecturer/staff, use your email ending in tarc.edu.my.</Typography>
+        <div className='login'>
+            <Box sx={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
+                <Stack spacing={3}>
+                    <Typography variant='h2' align='center' sx={{ fontWeight: 'bold' }}>TARSB<br />Kanteen</Typography>
+                    <Typography variant='body' align='center'>Log in with your Google account to get started!</Typography>
+                    <Stack direction='row' alignItems='center' spacing={1}>
+                        <InfoIcon /><Typography variant='caption'>If you are a TAR student or lecturer/staff, use your email ending in tarc.edu.my.</Typography>
                     </Stack>
 
-                    <Box display="flex" justifyContent="center">
-                        <Button startIcon={<GoogleIcon />} variant="contained" onClick={signInWithGoogle} >Login with Google</Button>
+                    <Box display='flex' justifyContent='center'>
+                        <Button startIcon={<GoogleIcon />} variant='contained' onClick={signInWithGoogle} >Login with Google</Button>
                     </Box>
 
                 </Stack>
--- src/error-pages/Offline.js
+++ src/error-pages/Offline.js
@@ -6,7 +6,10 @@ import Stack from '@mui/material/Stack'
 const Offline = () => {
     return (
         <div className="offline">
-            <Box sx={{ width: '100%' }} display="flex" justifyContent="center" alignItems="center" minHeight="calc(100vh - 16px)">
+            <Box sx={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
                 <Stack alignItems="center" spacing={2}>
                     <Typography variant="boduy">You are offline.</Typography>
                     <CloudOffIcon />
